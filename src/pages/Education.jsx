fc2f7f3b53673a9592d38593f1e4b96156e7440f
Amarillora <amarillora@students.nu-dasma.edu.ph>
2026-08-11 19:37:59 +0800

    updated

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -8,7 +8,7 @@ const Education = () => {
       title:
         "Bachelor of Science in Information Technology (Mobile & Web Applications)",
       school: "National University – Dasmariñas | 2022 – 2026",
-      grades: ["Cummulative General Weighted Average: 3.18"],
+      grades: ["Cummulative General Weighted Average:  3.18  (4 is the highest)"],
     },
     {
       title: "Senior High School (STEM Strand)",
